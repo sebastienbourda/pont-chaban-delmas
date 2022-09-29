05cf921151f0ec0ee8dfe7e77e61c8d0312310b5
Sebastien Bourda <32631203+sebastienbourda@users.noreply.github.com>
2022-09-29 15:14:26 +0200

    fix done

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,9 +6,9 @@
   <div class="about">
     <p>This website let you know if the bridge Jacques Chaban Delmas will
     be closed or not during the day.</p>
-    <p>It use <a href="https://opendata.bordeaux-metropole.fr/pages/accueil/" target="_blank">Bordeaux Open Data</a> to find informations.</p>
-    <p>It was created by <a href="www.linkedin.com/in/sebastien-bourda">Sebastien Bourda</a>, Freelance Creative Developer based in Bordeaux, France.</p>
-    <p>If you like the website, you can offer me a coffee <a href="https://www.buymeacoffee.com/778gnfcf9tx" target="_blank"></a> there.</p>
+    <p>It uses <a href="https://opendata.bordeaux-metropole.fr/pages/accueil/" target="_blank">Bordeaux Open Data</a> to find informations.</p>
+    <p>It has been created by <a href="https://www.linkedin.com/in/sebastien-bourda">Sebastien Bourda</a>, Freelance Creative Developer based in Bordeaux, France.</p>
+    <p>If you like the website, you can offer him a coffee <a href="https://www.buymeacoffee.com/778gnfcf9tx" target="_blank">there</a>.</p>
   </div>
   <Footer></Footer>
 </template>
