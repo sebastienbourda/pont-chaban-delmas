7bfd3d7197711c0c67292a15197f50798208f7b0
Sebastien Bourda <32631203+sebastienbourda@users.noreply.github.com>
2022-09-29 14:37:55 +0200

    add link to buy me a coffee

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,7 +7,8 @@
     <p>This website let you know if the bridge Jacques Chaban Delmas will
     be closed or not during the day.</p>
     <p>It use <a href="https://opendata.bordeaux-metropole.fr/pages/accueil/" target="_blank">Bordeaux Open Data</a> to find informations.</p>
-    <p>It was created by <a href="">Sebastien Bourda</a>, Freelance Creative Developer based in Bordeaux, France.</p>
+    <p>It was created by <a href="www.linkedin.com/in/sebastien-bourda">Sebastien Bourda</a>, Freelance Creative Developer based in Bordeaux, France.</p>
+    <p>If you like the website, you can offer me a coffee <a href="https://www.buymeacoffee.com/778gnfcf9tx" target="_blank"></a> there.</p>
   </div>
   <Footer></Footer>
 </template>
