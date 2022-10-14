66a75c73ebc8c218cfbd2c54b2b974d628dc2bdc
Sebastien Bourda <32631203+sebastienbourda@users.noreply.github.com>
2022-10-14 17:32:24 +0200

    modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pont-chaban-delmas/',
+  base: 'https://www.pontchabandelmas.info',
   plugins: [vue()],
   resolve: {
     alias: {
